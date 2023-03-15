9db46cd0d5918e6ec609a74fec6ba5afdc9907b7
NeRaffik <raf.fik@yandex.ru>
2023-03-15 16:31:50 +0300

    modified:   access.log 	modified:   constants/root.js 	modified:   css/index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ function render(){
 
 }
 
+spinnerPage.render();
+
 let CATALOG = [];
 
-render();
+//https://api.myjson.online/v1/records/d9537e83-d34f-44e2-9c27-85a0f8951740
 
 fetch('server/launch.json')
     .then(res => res.json())
     .then(body => {
         CATALOG = body;
+
+        //setTimeout(() => {
+			spinnerPage.handleClear();
+		//	render();
+		//}, 1000);
+
+        render();
     })
-    .catch(Error => {
-        console.log(Error);
+    .catch(() => {
+        spinnerPage.handleClear();
+    	errorPage.render();
     });
